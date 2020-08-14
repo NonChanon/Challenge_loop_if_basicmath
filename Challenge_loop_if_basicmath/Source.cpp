e8bde8e841a459e6cf3269ca73e8932f48eff219
CHANON KITBUNNADAECH <63010225@KMITL.AC.TH>
2020-08-15 00:37:49 +0700

    Add project files.

---
+++ Challenge_loop_if_basicmath/Source.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a, b, i, x = 0;
+	float sum = 0, sum1 = 0;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	if (a > b)
+	{
+		for (i = a; i >= b; i--)
+		{
+			printf("%d ", i);
+			sum += i;
+			sum1 += (i * i);
+			x += 1;
+		}
+		printf("\nAverage = %.1f", sum / x);
+		printf("\nSD = %.2f", sqrt((x * sum1 - (sum * sum)) / ((x - 1) * x)));
+	}
+	if (a < b)
+	{
+		for (i = a; i <= b; i++)
+		{
+			printf("%d ", i);
+			sum += i;
+			sum1 += (i * i);
+			x += 1;
+		}
+		printf("\nAverage = %.1f", sum / (b - a + 1));
+		printf("\nSD = %.2f", sqrt((x * sum1 - (sum * sum)) / ((x - 1) * x)));
+	}
+	return 0;
+}
